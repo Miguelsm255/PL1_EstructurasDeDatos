560a4279cc059e03832f7569b757cebce7df4333
Miguelsm255 <miguel.desandemoreno@gmail.com>
2024-11-11 22:15:47 +0100

    Terminado de comentar

--- Nucleo.cpp
+++ Nucleo.cpp
@@ -26,32 +26,32 @@ Nucleo::~Nucleo()
 
 }
 
-bool Nucleo::esVacio()
+bool Nucleo::esVacio()  // devuelve si el núcleo está vacío
 {
-    return libre;
+    return libre;   // devuelve si el núcleo está libre
 }
 
-void Nucleo::procesar(Proceso p)
+void Nucleo::procesar(Proceso p)    // procesa un proceso
 {
-    if (esVacio())
+    if (esVacio())  // si el núcleo está libre
     {
-        proceso = p;
-        libre = false;
+        proceso = p;    // el proceso del núcleo es el proceso que se le pasa
+        libre = false;  // el núcleo ya no está libre
     }
 }
 
-void Nucleo::quitarProceso()
+void Nucleo::quitarProceso()    // quita el proceso del núcleo
 {
-    if(!esVacio())
+    if(!esVacio())  // si el núcleo no está vacío
     {
-        proceso = Proceso();
-        libre = true;
+        proceso = Proceso();    // el proceso del núcleo es un proceso vacío
+        libre = true;           // el núcleo está libre
     }
 }   
 
-void Nucleo::mostrarNucleo()
+void Nucleo::mostrarNucleo()    // muestra el proceso del núcleo
 {
-    if(!esVacio())
+    if(!esVacio())  
     {
         cout << "      PID: " << proceso.PID << endl;
         cout << "      PPID: " << proceso.PPID << endl;
@@ -69,51 +69,51 @@ void Nucleo::mostrarNucleo()
 }
 
 
-int Nucleo::actualizar()
+int Nucleo::actualizar()    // actualiza el proceso
 {
-    if(!esVacio())
+    if(!esVacio())  // si el núcleo no está vacío
     {
-        proceso.tiempoEnSO += 1;
-        tiempoProcesado += 1;
+        proceso.tiempoEnSO += 1;    // aumento el tiempo que lleva en el SO
+        tiempoProcesado += 1;       // aumento el tiempo procesado en 1
         
-        if (proceso.tiempoEjecucion == tiempoProcesado)
+        if (proceso.tiempoEjecucion == tiempoProcesado) // si el tiempo de ejecución del proceso es igual al tiempo procesado
         {
-            int tiempoTotalProceso = proceso.tiempoEnSO;
+            int tiempoTotalProceso = proceso.tiempoEnSO;    // el tiempo total del proceso es el tiempo que lleva en el SO
             cout << "El proceso con PID " << proceso.PID << " ha terminado! Ha estado " << tiempoTotalProceso << " minutos en el SO."<< endl;
             cout << endl;
-            quitarProceso();
-            tiempoProcesado = 0;
-            return tiempoTotalProceso;
-        }else
+            quitarProceso();    // quito el proceso del núcleo
+            tiempoProcesado = 0;    // el tiempo procesado es 0
+            return tiempoTotalProceso;  // devuelvo el tiempo total del proceso
+        }else   // si el tiempo de ejecución del proceso no es igual al tiempo procesado
         {
-            return 0;
+            return 0;   // devuelvo 0
         }
     }else{
         return 0;
     }
 }
 
-int Nucleo::NdeProcesosEnCola()
+int Nucleo::NdeProcesosEnCola() // devuelve el número de procesos en la cola
 {
-    return colaProcesos->colaLongitud();
+    return colaProcesos->colaLongitud();    
 }
 
-void Nucleo::encolarProceso(Proceso p)
+void Nucleo::encolarProceso(Proceso p)  // encola un proceso
 {
     colaProcesos->encolarPrioridad(p);
 }
 
-Cola* Nucleo::obtenerColaNucleo()
+Cola* Nucleo::obtenerColaNucleo()   // devuelve la cola de procesos del núcleo
 {
     return colaProcesos;
 }
 
-void Nucleo::mostrarColaNucleo()
+void Nucleo::mostrarColaNucleo()    // muestra la cola de procesos del núcleo
 {
     colaProcesos->mostrarCola();
 }
 
-int Nucleo::getID()
+int Nucleo::getID() // devuelve el ID del núcleo
 {
     return ID;
 }
--- Pila.cpp
+++ Pila.cpp
@@ -19,83 +19,74 @@ bool Pila::esVacia()
     return cima == NULL;
 }
 
-void Pila::apilar(Proceso p)
+void Pila::apilar(Proceso p)    // apila un proceso
 {
-    pnodo nuevo = new NodoPila(p, cima);
+    pnodo nuevo = new NodoPila(p, cima);    // creo un nuevo nodo con el proceso y que apunte a la cima
     // comienzo de la pila nevo nodo
-    cima = nuevo;
+    cima = nuevo;   // la cima es el nuevo nodo
 }
 
-void Pila::desapilar()
+void Pila::desapilar()  // desapila un proceso
 {
     pnodo nodo; // puntero aux para manipular el nodo
-    if (cima)
+    if (cima)   // si la pila no está vacía
     {
-        nodo = cima;
-        cima = nodo->siguiente;
-        delete nodo;
+        nodo = cima;    // el nodo auxiliar es la cima
+        cima = nodo->siguiente; // la cima es el siguiente nodo
+        delete nodo;    // borro el nodo
     }
     
 }
 
-void Pila::mostrar()
-{
-    // if (esVacia())
-    // {
-    //     cout << "Pila vacia" << endl;
-    // }
-    // else
-    // {
-    //     cout << "Cima pila: " << cima->proceso.PID << endl;
-    // }
-    // return 0;
-
-    if (!esVacia())
+void Pila::mostrar()    // muestra la pila
+{   
+    if (!esVacia()) // si la pila no está vacía
     {
-        NodoPila *aux = cima;
-        while (aux != NULL)
+        NodoPila *aux = cima;   // nodo auxiliar para recorrer la pila
+        while (aux != NULL)     // mientras el nodo auxiliar no sea nulo
         {
-            cout << aux->proceso.PID << " ";
-            aux = aux->siguiente;
+            cout << aux->proceso.PID << " ";    // muestro el PID del proceso
+            aux = aux->siguiente;               // avanzo al siguiente nodo
         }
         cout << endl;
     }
-    else
+    else    // si la pila está vacía
     {
-        cout << "La pila está vacía." << endl;
+        cout << "La pila está vacía." << endl;  // muestro que la pila está vacía
     }
 }
 
-Proceso Pila::cimaPila()
+Proceso Pila::cimaPila()    // devuelve el proceso de la cima
 {
-    if (!esVacia())
+    if (!esVacia()) // si la pila no está vacía
     {
-        return cima->proceso;
+        return cima->proceso;   // devuelvo el proceso de la cima
     }
-    return Proceso();
+    return Proceso();   // si la pila está vacía devuelvo un proceso vacío
 }
 
-void Pila::insertarTiempo(Proceso proceso)
+void Pila::insertarTiempo(Proceso proceso)  // inserta un proceso en la pila según su tiempo de inicio
 {
-    if (esVacia())
+    if (esVacia())  // si la pila está vacía
     {
-        apilar(proceso);
+        apilar(proceso);    // apilo el proceso
     }
-    else
+    else    // si la pila no está vacía
     {
-        Pila *aux = new Pila();
-        while (!esVacia() && cimaPila().tiempoInicio <= proceso.tiempoInicio)
+        Pila *aux = new Pila(); // pila auxiliar
+        // mientras la pila no esté vacía y el tiempo de inicio del proceso sea menor o igual al tiempo de inicio del proceso de la cima
+        while (!esVacia() && cimaPila().tiempoInicio <= proceso.tiempoInicio)   
         {
-            aux->apilar(cimaPila());
-            desapilar();
+            aux->apilar(cimaPila());    // apilo el proceso de la cima en la pila auxiliar
+            desapilar();        // desapilo el proceso de la cima
         }
 
-        apilar(proceso);
+        apilar(proceso);    // apilo el proceso
 
-        while (!aux->esVacia())
+        while (!aux->esVacia()) // mientras la pila auxiliar no esté vacía
         {
-            apilar(aux->cimaPila());
-            aux->desapilar();
+            apilar(aux->cimaPila());    // apilo el proceso de la cima de la pila auxiliar
+            aux->desapilar();        // desapilo el proceso de la cima de la pila auxiliar
         }
         delete aux;
     }
