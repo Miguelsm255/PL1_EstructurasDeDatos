f4afd0a5e8e8e15cc0dc8001b87fb097f49a3491
Miguelsm255 <miguel.desandemoreno@gmail.com>
2024-12-04 21:33:27 +0100

    Terminar de realizar los cambios traídos desde Parte_II_(listas)
    como en la 2a parte se llamaban Lista y en esta tercera parte se llaman listaNucleos no se actualizaron todos los cambios en el merge. Ya están actualizados

--- include/ListaNucleos/ListaNucleos.cpp
+++ include/ListaNucleos/ListaNucleos.cpp
@@ -18,41 +18,15 @@ ListaNucleos::ListaNucleos(Nucleo nucleo, ListaNucleos *resto)
     longitud = resto->longitud + 1;
 }
 
+ListaNucleos::~ListaNucleos()
+{
+    
+}
+
+
 bool ListaNucleos::esVacia()
 {
     return vacia;
-}
-
-ListaNucleos* ListaNucleos::añadirIzquierda(Nucleo nucleo)    // añade un nodo a la izquierda
-{
-    return new ListaNucleos(nucleo, this);     // crea una lista nueva con el núcleo a la izquierda y la
-}                                       // lista antigua como resto y lo devuelve
-       
-
-ListaNucleos* ListaNucleos::añadirDerecha(Nucleo nucleo)   // añade un nodo a la derecha
-{
-    if (esVacia())  // si la lista está vacía
-    {
-        primeroListaNucleos = nucleo;  // el primer nodo es el núcleo
-        restoListaNucleosPtr = new ListaNucleos();    // el resto es una lista vacía
-        vacia = false;  // la lista ya no está vacía
-        longitud = 1;   // la longitud es 1
-    }
-    else    // si la lista no está vacía
-    {
-        ListaNucleos* p = this;    // creo un puntero a la lista
-
-        // mientras que el siguiente nodo no sea NULL y la lista no esté vacía (hasta que no llego al final)
-        while (p->restoListaNucleosPtr != NULL && !p->restoListaNucleosPtr->esVacia())    
-        {
-            p->longitud++; // aumento la longitud a cada uno de los nodos
-            p = p->restoListaNucleosPtr;   // avanzo en la lista
-            
-        }
-        p->longitud++;
-        p->restoListaNucleosPtr = new ListaNucleos(nucleo, new ListaNucleos());  // añado el núcleo al final de la lista
-    }
-    return this;    // devuelvo la lista
 }
 
 Nucleo ListaNucleos::primero(ListaNucleos lista)  // devuelve el primer nodo
@@ -64,6 +38,18 @@ Nucleo ListaNucleos::primero(ListaNucleos lista)  // devuelve el primer nodo
     else
     {
         return Nucleo();    // si la lista está vacía, devuelvo un núcleo vacío
+    }
+}
+
+Nucleo* ListaNucleos::primeroPtr() // devuelve el puntero al primer nodo
+{   
+    if (!esVacia()) // si la lista no está vacía
+    {
+        return &this->primeroListaNucleos;   // devuelvo el puntero al primer nodo
+    }
+    else
+    {
+        return NULL;    // si la lista está vacía, devuelvo NULL
     }
 }
 
@@ -79,6 +65,36 @@ ListaNucleos ListaNucleos::resto(ListaNucleos lista) // devuelve el resto de la 
     }
 }
 
+ListaNucleos* ListaNucleos::restoPtr(ListaNucleos lista) // devuelve el puntero al resto de la lista
+{
+    if (!esVacia()) // si la lista no está vacía
+    {
+        return lista.restoListaNucleosPtr;   // devuelvo el puntero al resto de la lista
+    }
+    else
+    {
+        return NULL;    // si la lista está vacía, devuelvo NULL
+    }
+}
+
+
+Nucleo* ListaNucleos::obtenerNodo(int posicion)    // obtiene un nodo de la lista
+{
+    if (!esVacia() && posicion <= longitud) // si la lista no está vacía y la posición es menor o igual a la longitud
+    {
+        ListaNucleos* p = this;    // creo un puntero a la lista
+        for (int i = 1; i < posicion; i++)  // recorro la lista hasta la posición
+        {
+            p = p->restoListaNucleosPtr;   // avanzo en la lista
+        }
+        return p->primeroPtr(); // devuelvo el puntero al primer nodo
+    }
+    else    // si la lista está vacía o la posición no existe
+    {
+        cout << "La posición no existe" << endl;    // muestro un mensaje de error
+        return NULL;    // devuelvo NULL
+    }
+}
 
 ListaNucleos* ListaNucleos::eliminarNodo(int posicion)    // elimina un nodo de la lista
 {
@@ -133,17 +149,94 @@ ListaNucleos* ListaNucleos::eliminarNodo(int posicion)    // elimina un nodo de 
     return this;    // devuelvo la lista
 }
 
-ListaNucleos* ListaNucleos::restoPtr(ListaNucleos lista) // devuelve el puntero al resto de la lista
+int ListaNucleos::longitudListaNucleos()  // devuelve la longitud de la lista
 {
-    if (!esVacia()) // si la lista no está vacía
-    {
-        return lista.restoListaNucleosPtr;   // devuelvo el puntero al resto de la lista
-    }
-    else
-    {
-        return NULL;    // si la lista está vacía, devuelvo NULL
-    }
+    return longitud;    // devuelvo la longitud
 }
+
+Nucleo* ListaNucleos::NucleoConMenosProcesos()
+{
+    // Compruebo cual es el núcleo con menos procesos en cola y encolo
+    Nucleo* menor = this->obtenerNodo(1); // por defecto cogemos el primero
+    for (int j = 1; j <= this->longitudListaNucleos(); j++) // por cada núcleo
+    {
+        // si el núcleo j tiene menos procesos en cola que el núcleo menor
+        if (this->obtenerNodo(j)->NdeProcesosEnCola() <= menor->NdeProcesosEnCola())
+        {
+            if (this->obtenerNodo(j)->NdeProcesosEnCola() == menor->NdeProcesosEnCola() && this->obtenerNodo(j)->esVacio())
+            {
+                cout << "El núcleo " << j << " tiene menos procesos en cola" << endl;
+                menor = this->obtenerNodo(j); // el núcleo j es el nuevo menor
+            }
+            else if (this->obtenerNodo(j)->NdeProcesosEnCola() < menor->NdeProcesosEnCola())
+            {
+                cout << "El núcleo " << j << " tiene menos procesos en cola" << endl;
+                menor = this->obtenerNodo(j); // el núcleo j es el nuevo menor
+            }
+        }
+    }
+    cout << endl;
+    return menor;
+}
+
+
+ListaNucleos* ListaNucleos::añadirIzquierda(Nucleo nucleo)    // añade un nodo a la izquierda
+{
+    return new ListaNucleos(nucleo, this); // crea una lista nueva con el núcleo a la izquierda y la
+}                                          // lista antigua como resto y lo devuelve
+       
+ListaNucleos* ListaNucleos::añadirDerecha(Nucleo nucleo)   // añade un nodo a la derecha
+{
+    if (esVacia())  // si la lista está vacía
+    {
+        primeroListaNucleos = nucleo;  // el primer nodo es el núcleo
+        restoListaNucleosPtr = new ListaNucleos();    // el resto es una lista vacía
+        vacia = false;  // la lista ya no está vacía
+        longitud = 1;   // la longitud es 1
+    }
+    else    // si la lista no está vacía
+    {
+        ListaNucleos* p = this;    // creo un puntero a la lista
+
+        // mientras que el siguiente nodo no sea NULL y la lista no esté vacía (hasta que no llego al final)
+        while (p->restoListaNucleosPtr != NULL && !p->restoListaNucleosPtr->esVacia())    
+        {
+            p->longitud++; // aumento la longitud a cada uno de los nodos
+            p = p->restoListaNucleosPtr;   // avanzo en la lista
+            
+        }
+        p->longitud++;
+        p->restoListaNucleosPtr = new ListaNucleos(nucleo, new ListaNucleos());  // añado el núcleo al final de la lista
+    }
+    return this;    // devuelvo la lista
+}
+
+
+bool ListaNucleos::nucleosVacios()
+{
+    // Compruebo si todos los núcleos están vacíos
+    bool vacio = true;
+    for (int j = 1; j <= this->longitudListaNucleos(); j++)
+    {
+        cout << "Núcleo " << j << " tiene " << this->obtenerNodo(j)->NdeProcesosEnCola() << " procesos en cola." << endl;
+        cout << "Núcleo " << j << " está vacío: ";
+        if (this->obtenerNodo(j)->esVacio())
+        {
+            cout << "Sí" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+        cout << endl;
+        if (this->obtenerNodo(j)->NdeProcesosEnCola() != 0 || !this->obtenerNodo(j)->esVacio())
+        {
+            vacio = false;
+        }
+    }
+    return vacio;
+}
+
 
 void ListaNucleos::mostrarListaNucleos(ListaNucleos lista)   // muestra la lista
 {
@@ -155,43 +248,133 @@ void ListaNucleos::mostrarListaNucleos(ListaNucleos lista)   // muestra la lista
     cout << endl;   // salto de línea
 }
 
-
-Nucleo* ListaNucleos::obtenerNodo(int posicion)    // obtiene un nodo de la lista
+void ListaNucleos::mostrarColasDeNucleos()
 {
-    if (!esVacia() && posicion <= longitud) // si la lista no está vacía y la posición es menor o igual a la longitud
-    {
-        ListaNucleos* p = this;    // creo un puntero a la lista
-        for (int i = 1; i < posicion; i++)  // recorro la lista hasta la posición
+    if (this->longitudListaNucleos() > 0){
+        for (int j = 1; j <= this->longitudListaNucleos(); j++)
         {
-            p = p->restoListaNucleosPtr;   // avanzo en la lista
+            cout << "Cola de núcleo " << j << ": ";
+            this->obtenerNodo(j)->mostrarColaNucleo();
+            
         }
-        return p->primeroPtr(); // devuelvo el puntero al primer nodo
-    }
-    else    // si la lista está vacía o la posición no existe
-    {
-        cout << "La posición no existe" << endl;    // muestro un mensaje de error
-        return NULL;    // devuelvo NULL
-    }
-}
-
-int ListaNucleos::longitudListaNucleos()  // devuelve la longitud de la lista
-{
-    return longitud;    // devuelvo la longitud
-}
-
-ListaNucleos::~ListaNucleos()
-{
-    
-}
-
-Nucleo* ListaNucleos::primeroPtr() // devuelve el puntero al primer nodo
-{   
-    if (!esVacia()) // si la lista no está vacía
-    {
-        return &this->primeroListaNucleos;   // devuelvo el puntero al primer nodo
+        cout << endl;
     }
     else
     {
-        return NULL;    // si la lista está vacía, devuelvo NULL
+        cout << "No hay núcleos" << endl;
+        cout << endl;
+    }
+}
+
+void ListaNucleos::eliminarNucleosVacios()
+{
+    // compruebo el valor de nucleosVacios. Si hay más de 2, elimino núcleos vacíos hasta que queden 2
+    for (int j = 1; j <= this->longitudListaNucleos(); j++) // por cada núcleo
+    {
+        // si está vacío y nucleosVacios es mayor que 2
+        if (this->obtenerNodo(j)->esVacio() && this->obtenerNodo(j)->obtenerColaNucleo()->esVacia() && this->longitudListaNucleos() > 2)
+        {
+            cout << "Elimino núcleo " << j << endl;
+            this->eliminarNodo(j); // elimino el núcleo
+        }
+    }
+}
+
+void ListaNucleos::actualizarColasyNucleos(int* sumaTiempos, int* procesosEjecutados)
+{
+    // actualizo los núcleos
+    for (int j = 1; j <= this->longitudListaNucleos(); j++) // por cada núcleo
+    {
+        // actualizar suma 1 al tiempo que lleva ejecutado y al tiempo en SO
+        // si el proceso ha terminado, devuelve el tiempo que ha estado en el SO
+        // si no devuelve 0
+        int tiempo = this->obtenerNodo(j)->actualizar(); 
+
+        // si el tiempo es distinto de 0, es que ha terminado, sumo 1 a procesosEjecutados
+        if (tiempo != 0)
+        {
+            *procesosEjecutados += 1;
+        }
+        *sumaTiempos += tiempo; // sumo el timepo que me ha devuelto al tiempo total
+    }
+
+    // actualizar las colas: esto solo suma 1 al tiempo en SO.
+    for (int j = 1; j <= this->longitudListaNucleos(); j++) // por cada nucleo
+    {
+        this->obtenerNodo(j)->obtenerColaNucleo()->actualizar(); // accedo a su cola y actualizo
+    }
+}
+
+void ListaNucleos::encolarEnElMenor(Pila* pila)
+{
+    cout << "-----PARA ENCOLAR COMPRUEBO CUAL ES EL NÚCLEO CON MENOS COLA-----" << endl;
+    this->mostrarColasDeNucleos(); // SOLO VISUAL
+
+    Nucleo* menor = NucleoConMenosProcesos(); // obtengo el núcleo con menos procesos en cola
+
+    if (menor->NdeProcesosEnCola() == 0 && menor->esVacio())
+    {
+        menor->procesar(pila->cimaPila()); // lo pongo a procesar directamente
+        cout << "Como no tiene cola y está vacío, lo meto en el núcleo." << endl;
+    }
+    else
+    {
+        menor->encolarProceso(pila->cimaPila()); // encolo el proceso en el núcleo menor
+        cout << "Encolo en el menor" << endl;
+    }
+    this->mostrarColasDeNucleos(); // SOLO VISUAL        
+    cout << endl;
+}
+
+void ListaNucleos::hacerHueco()
+{
+    cout << "-----COMPRUEBO SI LOS NÚCLEOS ESTÁN LLENOS-----" << endl;
+    cout << endl;
+    // Compruebo si están todos llenos
+    bool llenos = true;
+    for (int j = 1; j <= this->longitudListaNucleos(); j++) // por cada núcleo
+    {
+        if (this->obtenerNodo(j)->NdeProcesosEnCola() < 3) // si un nucleo tiene - de 3 procesos (no lleno)
+        {
+            llenos = false; // no están todos llenos
+            cout << "Núcleo " << j << " no está lleno, tiene " << this->obtenerNodo(j)->NdeProcesosEnCola() << " procesos en cola." << endl;
+            cout << "Cola de núcleo " << j << ": ";
+            this->obtenerNodo(j)->mostrarColaNucleo();
+            cout << endl;
+        }
+        
+    }
+
+    // Si están todos llenos, añado un nuevo núcleo
+    if (llenos)
+    {
+        this->añadirDerecha(Nucleo()); // añado núcleo nuevo
+        cout << "Están todos llenos, hemos añadido un nuevo núcleo" << endl;
+        cout << "Ahora hay " << this->longitudListaNucleos() << " núcleos" << endl;
+    }
+    cout << endl;
+}
+
+void ListaNucleos::desencolarYProcesar()
+{
+    // Si el nucleo está vacío y su lista tiene procesos esperando, pongo el primero a procesar
+    for (int j = 1; j <= this->longitudListaNucleos(); j++) // por cada núcleo
+    {
+        // aquí se hace la comprobación de si el núcleo está vacío y tiene procesos en cola
+        if (this->obtenerNodo(j)->esVacio() && !this->obtenerNodo(j)->obtenerColaNucleo()->esVacia())
+        {
+            cout << "Pongo a porcesar el proceso en el núcleo " << j << endl;
+            this->obtenerNodo(j)->procesar(this->obtenerNodo(j)->obtenerColaNucleo()->desencolar());
+        }
+    }
+    cout << endl;
+}
+
+void ListaNucleos::detallesProcesosEjecucion()
+{
+    for (int i = 1; i <= this->longitudListaNucleos(); i++) // por cada núcleo
+    {
+        cout << "- Proceso en ejecución en núcleo " << i << ": " << endl;
+        this->obtenerNodo(i)->mostrarNucleo(); // Imprime el proceso en ejecución
     }
 }
--- include/ListaNucleos/ListaNucleos.h
+++ include/ListaNucleos/ListaNucleos.h
@@ -1,6 +1,7 @@
 #ifndef LISTA_NUCLEOS_H
 #define LISTA_NUCLEOS_H
 #include "../Nucleo/Nucleo.h"
+#include "../Pila/Pila.h"
 #include <iostream>
 
 class ListaNucleos
@@ -14,16 +15,30 @@ public:
     ListaNucleos();
     ListaNucleos(Nucleo nucleo, ListaNucleos *resto = new ListaNucleos());
     ~ListaNucleos();
+
     bool esVacia();
+    Nucleo primero(ListaNucleos lista);
+    Nucleo* primeroPtr();
     ListaNucleos resto(ListaNucleos lista);
     ListaNucleos* restoPtr(ListaNucleos lista);
-    Nucleo primero(ListaNucleos lista);
+    
     Nucleo* obtenerNodo(int posicion);
     ListaNucleos* eliminarNodo(int posicion);
+    int longitudListaNucleos();
+    Nucleo* NucleoConMenosProcesos();
+
     ListaNucleos* añadirIzquierda(Nucleo nucleo);
     ListaNucleos* añadirDerecha(Nucleo nucleo);
-    int longitudListaNucleos();
-    Nucleo* primeroPtr();
+    
+    bool nucleosVacios();
+
     void mostrarListaNucleos(ListaNucleos lista);
+    void mostrarColasDeNucleos();
+    void eliminarNucleosVacios();
+    void actualizarColasyNucleos(int* sumaTiempos, int* procesosEjecutados);
+    void encolarEnElMenor(Pila* pila);
+    void hacerHueco();
+    void desencolarYProcesar();
+    void detallesProcesosEjecucion();
 };
 #endif // NODOCOLA_H
